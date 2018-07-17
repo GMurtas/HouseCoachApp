21e75fa8d4c1f266711378cb7aa93c1121bce2b5
GiuliaM <giuliamrts@gmail.com>
2018-07-17 20:12:11 +0200

    Gadgets layout, with buttons set to gone when clicked

--- app/src/main/java/com/example/giuliam/housecoach/GadgetsFragment.java
+++ app/src/main/java/com/example/giuliam/housecoach/GadgetsFragment.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.content.Context;
 
@@ -15,23 +16,14 @@ import java.util.List;
 
 public class GadgetsFragment extends android.support.v4.app.Fragment  {
 
-    List<ItemGadget> itemGadgetList;
-
     Context context;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_tasks, null);
-        ListView listView = view.findViewById(R.id.listView);
+        View view = inflater.inflate(R.layout.fragment_gadgets, null);
 
 
-        itemGadgetList = new ArrayList<>();
 
-        itemGadgetList.add(new ItemGadget("Happy dog", "dog", "Buy"));
-        itemGadgetList.add(new ItemGadget("Lazy cat", "cat", "Buy"));
-
-        listAdapter adapter = new listAdapter(context, itemGadgetList);
-        listView.setAdapter(adapter);
 
         return view;
         }
@@ -40,8 +32,22 @@ public class GadgetsFragment extends android.support.v4.app.Fragment  {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        view.findViewById(R.id.listView);
-        view.findViewById(R.id.gadgetsF);
+
+        final Button b1 = view.findViewById(R.id.b1);
+        final Button b2 = view.findViewById(R.id.b2);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               b1.setVisibility(View.GONE);
+            }
+        });
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                b2.setVisibility(View.GONE);
+            }
+        });
 
 
 
